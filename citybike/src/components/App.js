c2051b960ef57475954f68df913f005c7921650a
Alex Kimory <JustCodeandChill>
2020-07-31 15:30:23 -0500

    Creating entry index.js file, components folder with App.js and pages folder with LandingPage.js

---
+++ citybike/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import LandingPage from '../pages/LandingPage';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Route path="/" component={LandingPage} />
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+export default App;
