07aa1329d96767f2659b7bdea0b2dff8b284ec9e
Alex Kimory <JustCodeandChill>
2020-07-31 18:06:47 -0500

    Install Materialzie-css and create styles folder with LandingPage.css

--- citybike/src/components/App.js
+++ citybike/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
+import '../../node_modules/materialize-css/dist/css/materialize.min.css';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                This is some content
                 <BrowserRouter>
                     <Route path="/" component={LandingPage} />
                 </BrowserRouter>
