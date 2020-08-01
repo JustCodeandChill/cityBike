de897eb0f3177f13f3e964421bad16970892aecf
Alex Kimory <JustCodeandChill>
2020-07-31 19:16:54 -0500

    Adding state, add componentDidMount initialize location and set them to longitude and latitude

--- citybike/src/pages/LandingPage.js
+++ citybike/src/pages/LandingPage.js
@@ -1,19 +1,53 @@
 import React from "react";
 import "../styles/LandingPage.css";
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      longitude: 0,
+      latitude: 0,
+      city: "",
+    };
+  }
+
+  componentDidMount = () => {
+    let state = this;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log("in nat", this);
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+
+        state.setState({
+          longitude: long,
+          latitude: lat,
+        });
+      });
+    } else {
+      alert("Geolocation is not supported by this browser");
+    }
+  };
+
   render() {
     return (
       <div className="container">
         <h3>Welcome to FindACityBike</h3>
         <p className="inform-paragraph">
           Please turn on your Wifi and input the city where you want to find a
-          nearest citybike station
+          nearest citybike station. If error occurs, please reload and enable
+          location asking
         </p>
         <div className="dashboard-input">
           <input type="text" placeholder="City Name. Ex: New York" />
-          
         </div>
-        <button className="dashboard-search-button"> <i class="prefix material-icons">search</i>Search</button>
+        <button className="dashboard-search-button">
+          {" "}
+          <i className="material-icons">search</i>Search
+        </button>
+        <h5>
+          Current location: {this.state.latitude ? this.state.latitude : "null"}{" "} <br/>
+          {this.state.longitude ? this.state.longitude : "null"}
+        </h5>
       </div>
     );
   }
