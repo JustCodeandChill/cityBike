de92a59c40f9b6eb957e3c1eeeb6e89c42c1c322
Alex Kimory <JustCodeandChill>
2020-08-02 12:09:36 -0500

    Create HetId function

--- citybike/src/pages/LandingPage.js
+++ citybike/src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import "../styles/LandingPage.css";
 import {generateStationIdWithCountryandCitySpecifically} from '../utils/getStationIdThatMatchCountryandCitySearch';
 //components
 import Validate from '../components/FindCityandCountry';
+import Prompt from '../components/Validator';
+import MainValidator from '../components/MainValidator';
 
 class LandingPage extends React.Component {
   constructor(props) {
@@ -12,15 +14,14 @@ class LandingPage extends React.Component {
       longitude: 0,
       latitude: 0,
       city: "",
+      country: ""
     };
   }
 
   componentDidMount = async () => {
-    console.log(generateStationIdWithCountryandCitySpecifically());
     let state = this;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
-        console.log("in nat", this);
         let lat = position.coords.latitude;
         let long = position.coords.longitude;
 
@@ -34,6 +35,23 @@ class LandingPage extends React.Component {
     }
   };
 
+  handleChange = (e) => {
+    let choice = e.target.value || "";
+    choice = choice.split(", ")
+    let country = choice[0];
+    let countryArr = choice.slice(1);
+    let  city = countryArr.join(", ")
+
+    console.log('cc', country, city);
+    this.setState({
+        country : country,
+        city : city
+    })
+  };
+
+  handleClick = (e) => {
+
+  }
   render() {
     return (
       <div className="container">
@@ -45,16 +63,18 @@ class LandingPage extends React.Component {
         </p>
         <Validate />
         <div className="dashboard-input">
-          <input type="text" placeholder="City Name. Ex: New York" />
+          <input type="text" placeholder="City Name. Ex: New York" onChange={this.handleChange} />
         </div>
-        <button className="dashboard-search-button">
+        <button className="dashboard-search-button" onClick={this.handleClick}>
           {" "}
           <i className="material-icons">search</i>Search
         </button>
+
         <h5>
           Current location: {this.state.latitude ? this.state.latitude : "null"}{" "} <br/>
           {this.state.longitude ? this.state.longitude : "null"}
         </h5>
+        <MainValidator {...this.state} />
       </div>
     );
   }
--- citybike/src/utils/getStationIdThatMatchCountryandCitySearch.js
+++ citybike/src/utils/getStationIdThatMatchCountryandCitySearch.js
@@ -1,27 +1,86 @@
-import axios from 'axios'; 
-import _ from 'lodash';
-import {getNetworkData} from './common';
+import axios from "axios";
+import _ from "lodash";
+import { getNetworkData } from "./common";
 
-export const getStationIdThatMatchCountrySearch = (choice) => {
+export const getStationIdThatMatchCountryAndCitySearch = async (
+  country = "",
+  city = ""
+) => {
+  let data = await generateStationIdWithCountryandCitySpecifically();
+  let matches = [];
 
-}
+  _.map(data, (d) => {
+    if (
+      d.country.toLowerCase() === country.toLowerCase() &&
+      d.city.toLowerCase() === city.toLowerCase()
+    ) {
+      console.log(d.country.toLowerCase(), country, d.city.toLowerCase(), city);
+      matches.push(d);
+    }
+  });
+  let id = [];
+  _.map(matches, (match) => {
+    id.push(match.id);
+  });
+  console.log("in getid country and city", id);
+  return id;
+};
 
+export const getStationIdThatMatchCountrySearch = async (country = "") => {
+  let data = await generateStationIdWithCountryandCitySpecifically();
+  let matches = [];
+
+  _.map(data, (d) => {
+    if (d.country.toLowerCase() === country.toLowerCase()) {
+      console.log(d.country.toLowerCase(), country);
+      matches.push(d);
+    }
+  });
+  let id = [];
+  _.map(matches, (match) => {
+    id.push(match.id);
+  });
+  console.log("in getid country", id);
+  return id;
+};
+
+export const getStationIdThatMatchCitySearch = async (city = "") => {
+  let data = await generateStationIdWithCountryandCitySpecifically();
+  let matches = [];
+
+  _.map(data, (d) => {
+    if (d.city.toLowerCase() === city.toLowerCase()) {
+      console.log(d.city.toLowerCase(), city);
+      matches.push(d);
+    }
+  });
+  let id = [];
+  _.map(matches, (match) => {
+    id.push(match.id);
+  });
+  console.log("in getid city", id);
+  return id;
+};
 export const generateStationIdWithLocation = async () => {
-    const network = await getNetworkData();
-    let stationIdWithLocation = [];
-    _.map(network, ({id, location}) => {
-        stationIdWithLocation.push({id, location})
-    })
-    // console.log(network);
-    return stationIdWithLocation;
-}
+  const network = await getNetworkData();
+  let stationIdWithLocation = [];
+  _.map(network, ({ id, location }) => {
+    stationIdWithLocation.push({ id, location });
+  });
+
+  return stationIdWithLocation;
+};
 
 export const generateStationIdWithCountryandCitySpecifically = async () => {
-    const network = await getNetworkData();
-    let stationIdWithCountryandCity = [];
-    _.map(network, ({id, location}) => {
-        stationIdWithCountryandCity.push({id, country: location.country, city: location.city})
-    })
-    console.log(stationIdWithCountryandCity);
-    return stationIdWithCountryandCity
-}
+  const network = await getNetworkData();
+  let stationIdWithCountryandCity = [];
+  _.map(network, ({ id, location }) => {
+    stationIdWithCountryandCity.push({
+      id,
+      country: location.country,
+      city: location.city,
+    });
+  });
+
+  return stationIdWithCountryandCity;
+};
